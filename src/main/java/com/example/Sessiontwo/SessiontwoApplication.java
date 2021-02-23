016671e475f31dbec439baa562a34c33315adfaa
Meghna Bajoria <meghnabajoria@Meghnas-MacBook-Pro.local>
2021-02-23 14:14:29 +0530

    first commit

---
+++ src/main/java/com/example/Sessiontwo/SessiontwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sessiontwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessiontwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessiontwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Sessiontwo/SessiontwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sessiontwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessiontwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
