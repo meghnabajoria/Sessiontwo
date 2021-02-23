ef70c14e63d04333690f7b3e033e25649c67ef44
Meghna Bajoria <meghnabajoria@Meghnas-MacBook-Pro.local>
2021-02-23 17:49:48 +0530

    using mongodb

--- src/main/java/com/example/Sessiontwo/entity/Employee.java
+++ src/main/java/com/example/Sessiontwo/entity/Employee.java
@@ -2,26 +2,20 @@ package com.example.Sessiontwo.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 /**
  * @author meghna.bajoria
  * @since 23/02/21 2:31 PM
  **/
-@Entity //tells database that I will use this class to interact with the database
+@Document //tells database that I will use this class to interact with the database
 @Getter
 @Setter
 
 public class Employee {
-
-    @Id // tells that this is the primary key
-    @GenericGenerator(name = "employee_id_seq", strategy = "increment")
-    @GeneratedValue(generator = "employee_id_seq", strategy = GenerationType.AUTO)
+    @MongoId
     private long id;
 
     private String name;
