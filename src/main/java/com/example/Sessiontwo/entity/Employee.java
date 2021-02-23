79204d106723712658b25776408754fe1c15776a
Meghna Bajoria <meghnabajoria@Meghnas-MacBook-Pro.local>
2021-02-23 18:20:43 +0530

    using redis

--- src/main/java/com/example/Sessiontwo/entity/Employee.java
+++ src/main/java/com/example/Sessiontwo/entity/Employee.java
@@ -2,20 +2,22 @@ package com.example.Sessiontwo.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 
 /**
  * @author meghna.bajoria
  * @since 23/02/21 2:31 PM
  **/
-@Document //tells database that I will use this class to interact with the database
+
+@RedisHash//tells database that I will use this class to interact with the database
 @Getter
 @Setter
 
 public class Employee {
-    @MongoId
+    @Id
     private long id;
 
     private String name;
